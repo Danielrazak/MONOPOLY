09bcba5bd20aac3ed492ca4e5d1fc25257c4b0bb
Wan2729 <wanharithazdy12@gmail.com>
2023-10-10 22:19:23 +0800

    Nasib

---
+++ Main/Nasib.java
@@ -0,0 +1,144 @@
+package Main;
+
+import java.util.Random;
+
+public class Nasib {
+
+    private String name;
+    private int numOfFate;
+
+    public Nasib(String name, int numOfFate) {
+        this.name = name;
+        this.numOfFate = numOfFate;
+    }
+
+    public int randomizedFate() {
+        Random random = new Random();
+        return random.nextInt(numOfFate);
+    }
+
+    public void fateHappened() {
+
+    }
+
+    public void fateNumber1() {
+
+    }
+
+    public void fateNumber2() {
+
+    }
+
+    public void fateNumber3() {
+
+    }
+
+    public void fateNumber4() {
+
+    }
+
+    public void fateNumber5() {
+
+    }
+
+    public void fateNumber6() {
+
+    }
+
+    public void fateNumber7() {
+
+    }
+
+    public void fateNumber8() {
+
+    }
+
+    public void fateNumber9() {
+
+    }
+
+    public void fateNumber10() {
+
+    }
+
+    public void fateNumber11() {
+
+    }
+
+    public void fateNumber12() {
+
+    }
+
+    public void fateNumber13() {
+
+    }
+
+    public void fateNumber14() {
+
+    }
+
+    public void fateNumber15() {
+
+    }
+
+    public void fateNumber16() {
+
+    }
+
+    public void fateNumber17() {
+
+    }
+
+    public void fateNumber18() {
+
+    }
+
+    public void fateNumber19() {
+
+    }
+
+    public void fateNumber20() {
+
+    }
+
+    public void fateNumber21() {
+
+    }
+
+    public void fateNumber22() {
+
+    }
+
+    public void fateNumber23() {
+
+    }
+
+    public void fateNumber24() {
+
+    }
+
+    public void fateNumber25() {
+
+    }
+
+    public void fateNumber26() {
+
+    }
+
+    public void fateNumber27() {
+
+    }
+
+    public void fateNumber28() {
+
+    }
+
+    public void fateNumber29() {
+
+    }
+
+    public void fateNumber30() {
+
+    }
+
+}
