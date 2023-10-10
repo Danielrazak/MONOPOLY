65775666cf32916aa1a3409ea82212df3cb7e123
azim <u2102134@siswa.um.edu.my>
2023-10-11 00:07:04 +0800

    update class Nasib

--- Tile/Nasib.java
+++ Tile/Nasib.java
@@ -1,13 +1,36 @@
 package Tile;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 public class Nasib extends Tiles{
     private int numOfFate;
+    String fateList[];
 
     public Nasib(int numOfFate) {
         name = "fate";
-        this.numOfFate = numOfFate;
+
+        fateList = new String[numOfFate];
+        loadData();
+    }
+
+    private void loadData(){
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader("/src/nasibLog.txt"));
+
+            for(int i=0 ; i<numOfFate ; i++){
+                fateList[i] = fileReader.readLine();
+            }
+
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public int randomizedFate() {
@@ -16,7 +39,102 @@ public class Nasib extends Tiles{
     }
 
     public void fateHappened() {
+        int fate = randomizedFate();
 
+        int i=0;
+        if(fate == i++){
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
+        else if (fate == i++) {
+
+        }
     }
 
 }
