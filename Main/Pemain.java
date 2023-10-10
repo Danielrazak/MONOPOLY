2281deb327313756f2f7ff9d0d78d7009a03f8f3
Wan2729 <wanharithazdy12@gmail.com>
2023-10-10 23:50:17 +0800

    Hello

---
+++ Main/Pemain.java
@@ -0,0 +1,36 @@
+package Main;
+
+import Tiles.Tiles;
+
+public class Pemain {
+
+    private String name;
+    private float money;
+    private int currentTiles;
+    private Tiles currentTilesType;
+    private MonopolyBoard  board;
+    private boolean prisoner;
+
+    public Pemain(String name,float money,MonopolyBoard board){
+        this.name = name;
+        this.money = money;
+        prisoner = false;
+    }
+
+    public void throwDice(){
+
+    }
+
+    public void move(int toTiles){
+        currentTiles = toTiles;
+    }
+
+    public Tiles getTiles(){
+        currentTilesType = board.getTiles(currentTiles);
+        return currentTilesType;
+    }
+
+    public void optionByTiles(){
+
+    }
+}
