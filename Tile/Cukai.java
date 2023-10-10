4262b3f10ab52513bf2f124b11dd408c9e7c38bb
Wan2729 <wanharithazdy12@gmail.com>
2023-10-11 00:36:05 +0800

    Tambah Function Dalam Tiles

--- Tile/Cukai.java
+++ Tile/Cukai.java
@@ -1,13 +1,19 @@
 package Tile;
 
+import java.util.Scanner;
+
 public class Cukai {
 
     private String name;
     private float amountToPay;
+    private String[] option;
+    private int numberOfOption;
 
-    public Cukai(String name, float amountToPay) {
+    public Cukai(String name, int numberOfOption, float amountToPay) {
         this.name = name;
         this.amountToPay = amountToPay;
+        this.numberOfOption = numberOfOption;
+        option = new String[numberOfOption];
     }
 
 }
--- Tile/Tiles.java
+++ Tile/Tiles.java
@@ -2,7 +2,40 @@ package Tile;
 
 import Main.MonopolyBoard;
 
+import java.util.Scanner;
+
 public class Tiles{
     String name;
     MonopolyBoard board;
+
+    String[] option;
+    int numberOfOption;
+
+    public void setOption(int i) {
+        Scanner scanner = new Scanner(System.in);
+        char yesOrNo;
+        if (option[i] != null) {
+            System.out.println("Option " + i + " is already exist: " + option[i]);
+            System.out.println("Overwrite?");
+            System.out.println("[Y/N]");
+            yesOrNo = scanner.nextLine().toUpperCase().charAt(0);
+            switch (yesOrNo) {
+                case 'Y' -> option[i] = scanner.nextLine();
+            }
+        } else {
+            option[i] = scanner.nextLine();
+        }
+    }
+
+    public void displayOption() {
+        for (int i = 0; i < numberOfOption; i++) {
+            if (option[i] != null) {
+                System.out.println((i + 1) + ". " + option[i]);
+            }
+        }
+    }
+
+    public String[] getOption() {
+        return option;
+    }
 }
