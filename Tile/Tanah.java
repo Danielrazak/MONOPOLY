c314ca84e3c0f81462f6cae4dc99cb34228cf1cd
azim <u2102134@siswa.um.edu.my>
2023-10-11 00:09:39 +0800

    update class Nasib

--- Tile/Tanah.java
+++ Tile/Tanah.java
@@ -18,24 +18,24 @@ public class Tanah extends Tiles{
     public void buy(String player){
         owner = player;
         isOccupied = true;
-        //duit player berkurang
+        //TODO: duit player berkurang
     }
 
     public void sell(){
-        //duit player bertambah
+        //TODO: duit player bertambah
         isOccupied = false;
         owner = null;
     }
 
     public void rent(String player){
         if(!player.equals(owner)){
-            //player kena bayar rent
+            //TODO: player kena bayar rent
         }
     }
 
     public void buildHouse(int num){
         if((house += num) <= 4){
-            //player boleh beli rumah
+            //TODO: player boleh beli rumah
             house++;
             //duit player berkurang
         }
