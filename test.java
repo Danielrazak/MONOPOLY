0b5c9b0356a32af08e61a3cf8309bf74025b4c9b
azim <u2102134@siswa.um.edu.my>
2023-10-10 15:25:35 +0800

    tambah class

--- test.java
+++ test.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("den");
+public class test{
+    public static void main(String[] args) {
+        System.out.println("den");
+    }
 }
