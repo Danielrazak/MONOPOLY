bbb0ec1ae7b33dd8f0453f17cb8e1ac0fb0fd7e7
azim <u2102134@siswa.um.edu.my>
2023-10-11 00:16:11 +0800

    update TileManager

--- Tile/TilesManager.java
+++ Tile/TilesManager.java
@@ -1,4 +1,20 @@
 package Tile;
 
+import Main.MonopolyBoard;
+
 public class TilesManager {
+    Tiles[] tileList;
+    MonopolyBoard board;
+
+
+    public TilesManager(MonopolyBoard board) {
+        tileList = new Tiles[40];
+    }
+
+    private void setupTiles(){
+        //TODO: setup semua tiles according to properties
+
+        //contoh. Buat sampai tileList[39]
+        tileList[0] = new Tanah(board, "KLCC", 100, 10);
+    }
 }
