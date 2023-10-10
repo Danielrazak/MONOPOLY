0e3848d914264f778b4d89a77cdd3e78400b4242
azim <u2102134@siswa.um.edu.my>
2023-10-10 22:19:06 +0800

    scratch method dan variable tanah

---
+++ Main/GamePanel.java
@@ -0,0 +1,16 @@
+package Main;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    JPanel gp;
+
+    public GamePanel() {
+        this.gp = new JPanel();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
